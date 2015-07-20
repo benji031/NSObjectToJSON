a4acdb7b0df45d1ac7c37bbe4796e3527f861d6a
Benjamin DENEUX <benjamin.deneux@bananapps.fr>
2015-07-20 09:31:30 +0200

    Initial Commit

---
+++ NSObjectToJSONConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSObjectToJSONConverter
+//
+//  Created by Benjamin Deneux on 20/07/2015.
+//  Copyright (c) 2015 Bananapp's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
